b3636018b07c369ff59c72593cafd5cd5a4fbb4d
Legebrind <moyi88@gmail.com>
2022-07-29 17:42:02 +0000

    First delivery

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  function aleatorio(base) {
+    return Math.floor(Math.random() * base);
+  }
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  var aImprimir =
+    pronoun[aleatorio(2)] + adj[aleatorio(2)] + noun[aleatorio(2)] + ".com";
+  document.getElementById("mensaje").innerHTML = aImprimir;
+
   console.log("Hello Rigo from the console!");
 };
